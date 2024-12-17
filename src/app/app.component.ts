02266e03d89196559761f75e75f1865b492d41a3
Christina Bernhardt <christina.bernhardt@sopra-ft.com>
2024-12-17 10:36:57 +0100

    push unversioned files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent  implements OnInit {
   title = 'codingdojo';
+  course: string = '0';
 
   constructor(private httpClient: HttpClient) {
     // This service can now make HTTP requests via `this.http`.
@@ -19,7 +20,12 @@ export class AppComponent  implements OnInit {
   ngOnInit(): void {
     this.httpClient.get('https://data-api.binance.vision/api/v3/ticker/price?symbol=BTCUSDT').subscribe((data) => {
       console.log(data);
+      this.course = data;
+
     });
   }
+
+
+
 }
 
