b7c5650957e1d41707760cef5e0370508fe64fb2
u000921 <florina.sas@sopra-ft.com>
2024-12-17 09:59:17 +0100

    HttpClient init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'codingdojo';
+
+  constructor(private httpClient: HttpClient) {
+    // This service can now make HTTP requests via `this.http`.
+  }
+
+  ngOnInit(): void {
+
+  }
 }
+
